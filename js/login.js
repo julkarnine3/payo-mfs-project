21582422965d22478189e5aa0a123c7b91f8ae92
julkarnine3 <julkarninemdtarek@gmail.com>
2025-04-13 04:25:00 +0600

    Added re code for login

--- js/login.js
+++ js/login.js
@@ -1,19 +1,25 @@
-// console.log('login works');
-
-document.getElementById('login-btn').addEventListener('click', function (event) {
-    event.preventDefault();
-    const mobileNumber = document.getElementById('mobile-number').value;
-    const pinNumber=document.getElementById('pin-number').value;
-    const convertedPin=parseInt(pinNumber);
-    if(mobileNumber.length === 11){
-        if(convertedPin === 1234){
-            window.location.href="./main.html";
+// Catch the login button to interact
+document.getElementById("login-btn").addEventListener('click',
+    function (event) {
+        event.preventDefault();
+        // Getting mobile number and pin number
+        const mobileNumber = document.getElementById('mobile-number').value;        
+        const pinNumber=document.getElementById('pin-number').value;
+        // Convert the pinNumber into the integer
+        const convertedPin = parseInt(pinNumber);
+        // condition for verify pin and mobile number
+        
+        if(mobileNumber.length===11){
+            if(convertedPin===1234){
+                window.location.href='main.html';
+            }
+            else{
+                alert('Wrong pin number');
+            }
         }
         else{
-            alert('wrong pin');
+            alert("Invalid Mobile number");
         }
-    }
-    else{
-        alert('invalid phone number');
-    }
-})
+
+
+    })
